76135fd4f598dec2dd93258e0d6f05497c5194f5
RL Brown <rich@pineandvine.com>
2020-10-05 16:53:43 -0400

    remove __dirname as unnecc?

--- src/app.js
+++ src/app.js
@@ -40,12 +40,8 @@ MongoClient.connect(
     const db = client.db("radio-nachos");
     const collection = db.collection("playlists");
 
-    app.use("/react", express.static(__dirname + "/react"));
-    app.use(
-      express.static(__dirname + "/public", {
-        extensions: ["html"],
-      })
-    );
+    app.use("/react", express.static("/react"));
+    app.use(express.static("/public"));
     app
       .use(cors())
       .use(cookieParser())
